0049e2747a98532a321751f5a3fa8aadddd73fff
Hian Neiva <hianneiva@gmail.com>
2024-07-02 18:02:13 -0300

    Inclusion of bootstrap and basic site structure  - Inclusion of bootstrap files for script and style  - Inclusion of basic style structure with header, main content and footer

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
 const App = Vue.createApp({
-  setup() {
-    const message = Vue.ref('Hello Vue!');
-    return {
-      message
-    };
-  }
+    setup() {
+        const message = Vue.ref('Hello Vue!');
+        const start = Vue.ref('Start');
+        const footerMsg = Vue.ref('Information about the site goes here...');
+        return { message, start, footerMsg };
+    }
 });
